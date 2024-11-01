76e7b3f89f445df384a74983160659e830b6fd99
hasibmuhammad <hasibmu99@gmail.com>
2024-11-01 20:33:39 +0600

    generic type added

---
+++ src/generic.ts
@@ -0,0 +1,6 @@
+// Generic Name < Generic Type >
+type GenericArray<T> = Array<T>;
+
+const rollNumbers: GenericArray<number> = [1, 2, 3];
+const mentors: GenericArray<string> = ["shuvo", "Akash"];
+const boolArray: GenericArray<boolean> = [true, false, true];
